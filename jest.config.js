266fac857392de4d09a6b519dd1c38025e09d33b
Nathan Herrera <nathanherrera@gmail.com>
2020-09-09 21:44:54 -0700

    Adds jest script

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
-export default {
+module.exports = {
   moduleFileExtensions: [
     'js',
     'json',
     'vue'
   ],
   transform: {
-    '.*\\.(vue)$': 'vue-jest'
+    // process `*.vue` files with `vue-jest`
+    '.*\\.(vue)$': 'vue-jest',
+    // process `*.js` files with `babel-jest`
+    '.*\\.(js)$': 'babel-jest'
   },
   verbose: true
 }
