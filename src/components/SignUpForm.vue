8b4f5e08cbddd2491f5479983e351e02de2dc06e
Nathan Herrera <nathanherrera@gmail.com>
2020-09-13 23:15:30 -0700

    adds test/implementation for basic sign-up form component

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form @submit.prevent="signUp(form)"> 
+      <div>
+        <label>create a username</label>
+        <input v-model="form.username" type="text" data-field-type="username">
+      </div>
+      <div>
+        <label>what's your email?</label>
+        <input v-model="form.email" type="email" data-field-type="email">
+      </div>
+      <div>
+        <label>create a password</label>
+        <input v-model="form.password" type="password" data-field-type="password">
+      </div>
+      <div>
+        <label>confirm your password</label>
+        <input v-model="form.passwordConf" type="password" data-field-type="passwordConf">
+      </div>
+      <div>
+        <button type="submit">sign up</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        email: '',
+        password: '',
+        passwordConf: ''
+      }
+    }
+  },
+  methods: {  
+    signUp(formData) {
+      return formData
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
