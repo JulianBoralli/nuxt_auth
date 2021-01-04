66ec33e9b36a0e771600fc9891fa386367010f43
JulianBoralli <jboralli@icloud.com>
2021-01-04 14:46:44 -0800

    Define initial auth state.

--- src/plugins/railsAuthApi.js
+++ src/plugins/railsAuthApi.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 
 export default (context, inject) => {
-  console.log('Inject', context)
   const instance = axios.create({
     baseURL: 'http://127.0.0.1:3000/'
   })
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+
 export const state = () => ({
   auth: null
 })
